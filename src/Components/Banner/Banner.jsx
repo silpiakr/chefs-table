bbfe107640ef7d1fa38489bb28870295b62c8297
silpiAkter <silpiakter461@gmail.com>
2024-10-23 22:14:45 +0600

    added banner section

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -2,7 +2,19 @@ import React from 'react';
 
 const Banner = () => {
     return (
-        <div>
+        <div className='bg-cover bg-no-repeat rounded-3xl md:w-full  h-full md:h-[35.5rem] mb-10' style={{
+            backgroundImage:'url(https://i.ibb.co.com/7rrk2rc/hero-bg.png)'
+        }}>
+            <div className='w-9/12 mx-auto flex flex-col justify-center items-center text-center h-full space-y-5 py-4'>
+                <h1 className='font-bold text-white md:text-[3.25rem]'>Discover an exceptional cooking class tailored for you!</h1>
+                <p className='text-lg text-white font-normal'>
+                    Whether you are a beginner or a seasoned chef, our expert-led sessions offer personalized quidance and inspriation. Join us for an enriching culinary expreience that is sure to ignite your passion fot food and cooking.
+                </p>
+                <div className='flex gap-4 text-xl font-semibold'>
+                    <button className='btn bg-green-400 text-white rounded-full border-none'>Explore Now</button>
+                    <button className='btn btn-outline text-white hover:bg-green-400 rounded-full'>Our Feedback</button>
+                </div>
+            </div>
             
         </div>
     );
--- src/Components/Recipies/Recipes.jsx
+++ src/Components/Recipies/Recipes.jsx
@@ -2,10 +2,13 @@ import React from 'react';
 
 const Recipes = () => {
     return (
-        <div>
-            <h2>Our Recipes</h2>
+        <div className='text-center my-8 w-8/12 mx-auto space-y-4'>
+            <h2 className='font-semibold text-5xl text-gray-800'>Our Recipes</h2>
+            <p className='text-base text-gray-600'>
+                Explore our curated recipes for culinary inspireation. Form clasic comfort foods to gourmet delights, we have got something to satisfy every craving. Elevate your cooking with our easy-to-follow recipes, perfect for any occasion
+            </p>
         </div>
     );
-};
+}
 
 export default Recipes;
